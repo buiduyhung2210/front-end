20d10ab4cb3543b9d2a234a8b0e4384e592e3e8c
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-06-03 13:03:05 +0700

    Update

--- frontend-kingfood/src/Components/CardProductMenuPage.js
+++ frontend-kingfood/src/Components/CardProductMenuPage.js
@@ -5,20 +5,29 @@ import cardData from '../Datas/CardProductMenuPageData';
 
 function CardProductMenuPage() {
   return (
-    <div className={styles.cardContainer}>
-      {cardData.map(item => (
-        <Card key={item.id}>
-          <Card.Img className={styles.cardImg} variant="top" src={item.imgSrc} />
-          <Card.Body className="card-body">
-            <div>
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Text>
-                {item.text}
-              </Card.Text>
-            </div>
-          </Card.Body>
-        </Card>
-      ))}
+    <div className='container'>
+      <div className={styles.cardContainer}>
+        {cardData.map(item => (
+          <Card key={item.id} className={styles.card}>
+            <div className={styles.discountBadge}>10% OFF</div>
+            <Card.Img className={styles.cardImg} variant="top" src={item.imgSrc} />
+            <Card.Body className={styles.cardBody}>
+              <div className={styles.cardHeader}>
+                <div className={styles.cardTitle}>{item.title}</div>
+                <div className={styles.ratingBadge}>{item.rating} ★</div>
+              </div>
+              <div className={styles.cardTextRow}>
+                <div className={styles.cardText}>{item.description}</div>
+                <div className={styles.footerLeft}>{item.price}</div>
+              </div>
+              <div className={styles.cardFooter}>
+                <div className={styles.status}>{item.status}</div>
+                <div className={styles.footerRight}>{item.time}</div>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- frontend-kingfood/src/Datas/CardProductMenuPageData.js
+++ frontend-kingfood/src/Datas/CardProductMenuPageData.js
@@ -1,24 +1,34 @@
-// data.js
-const CardProductMenuPageData = [
-    {
-      id: 1,
-      title: 'Card Title 1',
-      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png'
-    },
-    {
-      id: 2,
-      title: 'Card Title 2',
-      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png'
-    },
-    {
-      id: 3,
-      title: 'Card Title 3',
-      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png'
-    },
-  ];
-  
-  export default CardProductMenuPageData;
-  
+const cardData = [
+  {
+    id: 1,
+    title: 'Lẩu Bò Vị Thái',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png',
+    rating: 4.2,
+    description: 'Lẩu 2 ngăn',
+    price: '1.300.000 Vnd / Set',
+    status: 'Tạm dừng',
+    time: '10 phút'
+  },
+  {
+    id: 2,
+    title: 'Lẩu Cua Sống',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png',
+    rating: 4.2,
+    description: 'Lẩu 2 Ngăn',
+    price: '1.300.000 Vnd / Set',
+    status: 'Tạm dừng',
+    time: '10 phút'
+  },
+  {
+    id: 3,
+    title: 'Burger King',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_the_NSDAP_(1920%E2%80%931945).svg/1920px-Flag_of_the_NSDAP_(1920%E2%80%931945).svg.png',
+    rating: 4.2,
+    description: 'Đồ Ăn Nhanh',
+    price: '100.000 Vnd / Set',
+    status: 'Tạm dừng',
+    time: '10 phút'
+  }
+];
+
+export default cardData;
