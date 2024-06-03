01f3b437ae5ad637436c409051695036c624667e
manh0107 <phamquangmanh0107@gmail.com>
2024-06-03 15:06:43 +0700

    upload profile page

--- frontend-kingfood/src/index.js
+++ frontend-kingfood/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from './Pages/Home';
+//import Home from './Pages/Home';
+import Profile from './Pages/Profile';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Profile />
   </React.StrictMode>
 );
 
